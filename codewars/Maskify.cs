2d087a671fbe40c79b6de39b7d54526b58655959
r1b0k <76029499+r1b0k@users.noreply.github.com>
2020-12-21 10:36:29 +0200

    Add files via upload

---
+++ codewars/Maskify.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAppForC01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(maskify("my name is george and i am rich!"));
+            
+            static string maskify(string n)
+            {
+                int getLength = n.Length;
+                var aStringBuilder = new StringBuilder(n);
+
+                if (getLength >= 5)
+                {
+                    string replaceWith = new string('#', getLength - 4);
+
+                    aStringBuilder.Remove(0, getLength - 4);
+                    aStringBuilder.Insert(0, replaceWith);
+                    n = aStringBuilder.ToString();                            
+                    
+                }
+                else
+                {
+                    goto End;
+                }
+                End:
+                return n;
+
+            }
+        }
+    }
+}
