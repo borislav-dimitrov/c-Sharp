98c4e79cc0abb297f0eb1237d08f1c4fda571b8d
r1b0k <76029499+r1b0k@users.noreply.github.com>
2020-12-26 19:31:55 +0200

    Add files via upload

---
+++ MyFirstGame/gameapp/Drawing.cs
@@ -0,0 +1,130 @@
+﻿using My_app_lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public class Drawing
+    {
+        private int Width = 820;
+        private int Height = 650;
+
+
+        public void drawCurrLocation(System.Windows.Controls.Canvas drawScene, Location currLocation)
+        {
+
+            drawScene.Width = 820;
+            drawScene.Height = 650;
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + $"\\images\\{currLocation.Name.ToString()}.jpg"));
+            drawScene.Background = ib;
+        }
+
+        public void drawMap(System.Windows.Controls.Canvas drawScene, Player _p1)
+        {
+            
+            clearCanvas(drawScene);
+
+            drawScene.Width = 820;
+            drawScene.Height = 650;
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\images\\Map.png"));
+            drawScene.Background = ib;
+
+            drawStormwindOnMap(drawScene, _p1);
+            drawHomeOnMap(drawScene, _p1);
+            drawGoldShireOnMap(drawScene, _p1);
+        }
+
+        #region drawStormwindOnMap
+        private void drawStormwindOnMap(System.Windows.Controls.Canvas drawScene, Player _p1)
+        {
+            Rectangle rectangle = new Rectangle { Width = 150, Height = 100, };
+            if (_p1.currentLocation.Name == "Storm_Wind")
+            {
+                rectangle.Fill = Brushes.LimeGreen;
+            }
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = Brushes.Black;
+            
+            drawScene.Children.Add(rectangle);
+            System.Windows.Controls.Canvas.SetLeft(rectangle, 1 * (100 + 2));
+            System.Windows.Controls.Canvas.SetTop(rectangle, 1 * (150 + 2));
+
+            TextBlock text = new TextBlock { Width = 130, Height = 25, };
+            text.Text = "Storm Wind";
+
+            drawScene.Children.Add(text);
+            System.Windows.Controls.Canvas.SetLeft(text, 1 * (115 + 2));
+            System.Windows.Controls.Canvas.SetTop(text, 1 * (190 + 2));
+        }
+        #endregion
+
+        #region drawHomeOnMap
+        private void drawHomeOnMap(System.Windows.Controls.Canvas drawScene, Player _p1)
+        {
+            Rectangle rectangle = new Rectangle { Width = 150, Height = 100, };
+            if (_p1.currentLocation.Name == "Home")
+            {
+                rectangle.Fill = Brushes.LimeGreen;
+            }
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = Brushes.Black;
+            
+            drawScene.Children.Add(rectangle);
+            System.Windows.Controls.Canvas.SetLeft(rectangle, 1 * (300 + 2));
+            System.Windows.Controls.Canvas.SetTop(rectangle, 1 * (110 + 2));
+
+            TextBlock text = new TextBlock { Width = 130, Height = 25, };
+            text.Text = "Home";
+
+            drawScene.Children.Add(text);
+            System.Windows.Controls.Canvas.SetLeft(text, 1 * (315 + 2));
+            System.Windows.Controls.Canvas.SetTop(text, 1 * (150 + 2));
+        }
+        #endregion
+
+        #region drawGoldShireOnMap
+        private void drawGoldShireOnMap(System.Windows.Controls.Canvas drawScene, Player _p1)
+        {
+            Rectangle rectangle = new Rectangle { Width = 150, Height = 100, };
+            if (_p1.currentLocation.Name == "Gold_Shire")
+            {
+                rectangle.Fill = Brushes.LimeGreen;
+            }
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = Brushes.Black;
+
+            drawScene.Children.Add(rectangle);
+            System.Windows.Controls.Canvas.SetLeft(rectangle, 1 * (300 + 2));
+            System.Windows.Controls.Canvas.SetTop(rectangle, 1 * (250 + 2));
+
+            TextBlock text = new TextBlock { Width = 130, Height = 25, };
+            text.Text = "Gold Shire";
+
+            drawScene.Children.Add(text);
+            System.Windows.Controls.Canvas.SetLeft(text, 1 * (315 + 2));
+            System.Windows.Controls.Canvas.SetTop(text, 1 * (290 + 2));
+        }
+        #endregion
+
+        #region closeMap
+
+        public void clearCanvas(System.Windows.Controls.Canvas drawScene)
+        {
+            drawScene.Background = Brushes.White;
+            drawScene.Children.Clear();
+            
+        }
+        #endregion
+    }
+}
---
+++ MyFirstGame/gameapp/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using My_app_lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Player _p1;
+        public Drawing _draw = new Drawing();
+        
+        public MainWindow()
+        {
+            WorldBuilder newWorld = new WorldBuilder();
+            newWorld.buildWorld();
+            
+
+            InitializeComponent();
+            
+            gameScreen.Effect = new BlurEffect();
+            charCreation.Visibility = Visibility.Visible;
+            
+        }
+
+
+
+
+        #region updatePlayerInfo
+
+        private void updatePlayerInfo(Player _p1)
+        {
+
+            lblPlayerName.Content = _p1.Name.ToString();
+            lblPlayerHP.Content = _p1.CurrHP.ToString();
+            lblPlayerMinDMG.Content = _p1.MinDMG.ToString();
+            lblPlayerMaxDMG.Content = _p1.MaxDMG.ToString();
+            lblPlayerExp.Content = _p1.CurrEXP.ToString();
+            lblPlayerLvl.Content = _p1.Level.ToString();
+        }
+        #endregion
+
+        #region sendAtHome
+
+        private void sendAtHome(Player _p1)
+        {
+            
+            _p1.currentLocation = WorldBuilder.LocationByID(WorldBuilder.LOC_ID_HOME);
+            updateLocation(_p1);
+            _p1.CurrHP = _p1.MaxHP / 2;
+            
+        }
+        #endregion
+
+        #region updateLocationInfo
+
+        private void updateLocation(Player _p1)
+        {
+            lblLocation.Content = _p1.currentLocation.Name.ToString() + " - " +
+                _p1.currentLocation.Description.ToString();
+            drawLocation(_p1);
+        }
+
+
+        #region drawLocation
+
+        private void drawLocation(Player _p1)
+        {
+            Drawing drawing = new Drawing();
+            drawing.drawCurrLocation(drawScene, _p1.currentLocation);
+        }
+        #endregion
+
+        #endregion
+
+        #region characterCreation
+
+        private void btnCreateChar_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCharName.Text == "")
+            {
+                MessageBox.Show("Input a name please");
+            }
+            else
+            {
+                _p1 = new Player(1, tbCharName.Text.ToString(), 50, 50, 1, 3, 0, 1);
+                updatePlayerInfo(_p1);
+                sendAtHome(_p1);
+                charCreation.Visibility = Visibility.Hidden;
+                gameScreen.Effect = null;
+            }
+            
+
+        }
+
+        #endregion
+
+        #region showMap
+
+        private void showMap()
+        {
+            _draw.drawMap(drawScene,_p1);
+        }
+        #endregion
+
+        private void btnMap_Click(object sender, RoutedEventArgs e)
+        {
+            showMap();
+        }
+
+        private void drawScene_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _draw.clearCanvas(drawScene);
+            updateLocation(_p1);
+        }
+
+        private void btnMoveNorth_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMoveWest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMoveSouth_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
