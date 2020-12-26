720345cbcd684e6f8cb034b0123648433595abd9
r1b0k <76029499+r1b0k@users.noreply.github.com>
2020-12-26 19:32:56 +0200

    Add files via upload

---
+++ MyFirstGame/game_lib/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace My_app_lib
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        
+
+        public Location(int id, string name, string description)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+        }
+
+    }
+}
